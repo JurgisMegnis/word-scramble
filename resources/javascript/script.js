a638e8cee2c550bbfaf4f5e51690d2875921ea58
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2024-12-18 12:24:48 +0200

    Added the game over screen with feature of starting new game, refactored some of the code for better practice

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,20 +1,22 @@
-/* SELECTORS */
-
-const getWordBtn = document.getElementById("get-word");
-const getWordBtnIcon = document.querySelector('.secondary svg');
-const submitBtn = document.getElementById('submit');
-const wordDisplay = document.querySelector(".word");
-const inputs = document.querySelectorAll(".inputs input");
-const triesIndicators = document.querySelectorAll('ol li');
-const triesMessage = document.querySelector('.tries-label');
-const wordsGuessed = document.querySelector('.words-guessed');
-const gameContainer = document.querySelector('.game-container');
-const inputsContainer = document.querySelector('.inputs');
-
-/* VARIABLES */
+/*  VARIABLES & SELECTORS */
 
 const url = "https://random-word-api.vercel.app/api?words=1&length=6";
 let originalWord; // variable for the original word to compare with the guess
+
+const DOM = {
+    gameContainer: document.querySelector('.game-container'),
+    getWordBtn: document.getElementById("get-word"),
+    submitBtn: document.getElementById('submit'),
+    wordDisplay: document.querySelector(".word"),
+    inputs: document.querySelectorAll(".inputs input"),
+    triesIndicators: document.querySelectorAll('ol li'),
+    triesMessage: document.querySelector('.tries-label'),
+    wordsGuessed: document.querySelector('.words-guessed'),
+    inputsContainer: document.querySelector('.inputs'),
+    newGameBtn: document.getElementById('reset'),
+    gameOverOverlay: document.querySelector('.game-over'),
+    gameOverMessage: document.querySelector('.game-over-counter')
+}
 
 /* OBJECTS */
 
@@ -99,19 +101,31 @@ class InputHandler {
     /* the guess against the originalWord */
     checkInput() {
         let guess = this.getGuess();
+        if (!guess) {
+            AnimationUtlis.shake(DOM.inputsContainer);
+            return;
+        } 
+        
         if (guess && guess === originalWord) {
-            statusMessage("Correct!", "success");
-            inputs.forEach(input => input.value = '');
+            NotificationUtils.showStatus("Correct!", "success");
+            DOM.inputs.forEach(input => input.value = '');
             displayTheWord();
             scoreTracker.trackGuess();
-        } else if (guess && scoreTracker.mistakes === scoreTracker.triesIndicators.length-1) {
-            alert(`Game over! Your score: ${scoreTracker.guesses}`);
-        } else if (guess && guess != originalWord) {
-            statusMessage("Not the word", "error");
-            scoreTracker.mistakes++;
-            scoreTracker.trackMistake();
-            shakeAnimation();
+            return;
         } 
+        
+        if (scoreTracker.mistakes === scoreTracker.triesIndicators.length-1) {
+            DOM.gameOverOverlay.hidden = false;
+            DOM.gameOverMessage.textContent = `You guessed ${scoreTracker.guesses} words`;
+            return;
+
+        }
+        
+        NotificationUtils.showStatus("Not the word", "error");
+        scoreTracker.mistakes++;
+        scoreTracker.trackMistake();
+        AnimationUtlis.flashInputs(DOM.inputs);
+        AnimationUtlis.shake(DOM.inputsContainer);
     }
 }
 
@@ -133,6 +147,15 @@ class ScoreTracker {
         }
     }
 
+    /* reset mistake indicators on a new game start */
+    resetMistakes() {
+        this.mistakes = 0;
+        this.updateTriesMessage();
+        DOM.triesIndicators.forEach( (indicator) => {
+            indicator.classList.remove('filled');
+        })
+    }
+
     /* updates the Tries(x/x) text */
     updateTriesMessage() {
         const totalTries = this.triesIndicators.length;
@@ -144,77 +167,140 @@ class ScoreTracker {
         this.guesses++;
         this.wordsGuessed.textContent = this.guesses;
     }
+
+    resetGuess() {
+        this.guesses = 0;
+        this.wordsGuessed.textContent = this.guesses;
+    }
+}
+
+/* animation related functions */
+const AnimationUtlis = {
+    /* add shake animation to an element */
+    shake(element) {
+    element.classList.add('shake');
+    setTimeout(() => {
+        element.classList.remove('shake')
+    }, 500); // matches animation duration
+    },
+
+    /* add animation flashing input fields red */
+    flashInputs(inputs) {
+        inputs.forEach(input => {
+            input.classList.add('flash');
+        });
+    
+        setTimeout(() => {
+            inputs.forEach(input => {
+                input.classList.remove('flash');
+            });
+        }, 1000);
+    },
+
+    /* assign animate class to the getWordBtnIcon */
+    rotateIcon() {
+        const getWordBtnIcon = document.querySelector('.secondary svg');
+        getWordBtnIcon.classList.add('animate');
+        setTimeout(() => {
+            getWordBtnIcon.classList.remove('animate')
+        }, 500)
+    }
+}
+
+/* notification related functions */
+const NotificationUtils = {
+    /* generating a toast for error messages */
+    showError(message) {
+        const errorDiv = document.createElement('div');
+        errorDiv.className = 'error-message';
+        errorDiv.textContent = message;
+        document.body.appendChild(errorDiv);
+
+        // remove it after 3s
+        setTimeout(() => {
+            errorDiv.remove();
+        }, 5000);
+    },
+
+    /* status message */
+    showStatus(message, type = 'success') {
+        const toast = document.createElement('div');
+        toast.className = `toast-${type}`;
+        toast.textContent = message;
+        DOM.gameContainer.appendChild(toast);
+
+        setTimeout(() => {
+            toast.remove();
+        }, 1500);
+    }
 }
 
 /* INSTANCES */
 
 const wordScrambler = new WordScrambler(); // object (instance) of the WordScrambler class
-const inputHandler = new InputHandler(inputs); // instance of the InputHandler class
-const scoreTracker = new ScoreTracker(triesIndicators, triesMessage, wordsGuessed); // instance of the ScoreTracker class
+const inputHandler = new InputHandler(DOM.inputs); // instance of the InputHandler class
+const scoreTracker = new ScoreTracker(DOM.triesIndicators, DOM.triesMessage, DOM.wordsGuessed); // instance of the ScoreTracker class
 
 scoreTracker.updateTriesMessage(); // initial setup call
 
 /* EVENT LISTENERS */
 
 document.addEventListener("DOMContentLoaded", displayTheWord); // generate scrambled word on document load
-getWordBtn.addEventListener("click", displayTheWord); // generate scrambled word on a click
-getWordBtn.addEventListener('click', animateIcon); // assign animate class to the getWordBtnIcon
-submitBtn.addEventListener('click', () => inputHandler.checkInput()); // submits the guess
+DOM.getWordBtn.addEventListener("click", displayTheWord); // generate scrambled word on a click
+DOM.getWordBtn.addEventListener('click', AnimationUtlis.rotateIcon); // assign animate class to the getWordBtnIcon
+DOM.submitBtn.addEventListener('click', () => inputHandler.checkInput()); // submits the guess
 
-inputs.forEach((input, index) => {
+DOM.inputs.forEach((input, index) => {
     input.addEventListener('input', () => inputHandler.handleInput(input, index)); 
     input.addEventListener('keydown', (e) => {
         inputHandler.handleBackspace(e, input, index)
         // add enter key handling to submit a guess
-        if (e.key === 'Enter' && inputHandler.getGuess()) {
+        if (e.key === 'Enter') {
             inputHandler.checkInput();
         }
     }); 
     input.addEventListener('focus', () => inputHandler.handleFocus(index)); 
 });
+DOM.newGameBtn.addEventListener('click', resetGame);
 
 /* EVENT HANDLERS */
 
 /* displays the scrambled word on a click */
 async function displayTheWord() {
-    getWordBtn.disabled = true; // disable button during load
-    wordDisplay.textContent = "Loading...";
-    inputs[0].focus();
+    DOM.getWordBtn.disabled = true; // disable button during load
+    DOM.wordDisplay.textContent = "...";
+    DOM.inputs[0].focus();
 
     try {
         originalWord = await wordScrambler.getWord();
         const scrambledWord = wordScrambler.scrambleTheWord(originalWord);
-        wordDisplay.textContent = scrambledWord;
+        DOM.wordDisplay.textContent = scrambledWord;
         console.log(originalWord);
     } catch(error) {
         console.error('Error in displayTheWord:', error);
-        wordDisplay.textContent = "";
+        DOM.wordDisplay.textContent = "";
         // showing specific error messages based on the error type
         if(error.message.includes('HTTP error')) {
-            showError("Unable to connect to server. Please try again later.");
+            NotificationUtils.showError("Unable to connect to server. Please try again later.");
         } else if (error.message.includes('No word received')) {
-            showError("No word available. Please try again.");
+            NotificationUtils.showError("No word available. Please try again.");
         } else {
-            showError("An unexpected error occurred. Please try again later.");
+            NotificationUtils.showError("An unexpected error occurred. Please try again later.");
         }
     } finally {
-        getWordBtn.disabled = false;
+        DOM.getWordBtn.disabled = false;
     }
 };
 
 /* HELPER FUNCTIONS */
 
-/* generating a toast for error messages */
-function showError(message) {
-    const errorDiv = document.createElement('div');
-    errorDiv.className = 'error-message';
-    errorDiv.textContent = message;
-    document.body.appendChild(errorDiv);
-
-    // remove it after 3s
-    setTimeout(() => {
-        errorDiv.remove();
-    }, 5000);
+/* start new game */
+function resetGame() {
+    DOM.gameOverOverlay.hidden = true;
+    DOM.inputs.forEach(input => input.value = '');
+    displayTheWord();
+    scoreTracker.resetMistakes();
+    scoreTracker.resetGuess();
 }
 
 /* status message */
@@ -222,27 +308,9 @@ function statusMessage(message, type = 'success') {
     const toast = document.createElement('div');
     toast.className = `toast-${type}`;
     toast.textContent = message;
-    gameContainer.appendChild(toast);
+    DOM.gameContainer.appendChild(toast);
 
     setTimeout(() => {
         toast.remove();
-    }, 1100);
-}
-
-/* assign animate class to the getWordBtnIcon */
-function animateIcon() {
-    getWordBtnIcon.classList.add('animate');
-    setTimeout(() => {
-        getWordBtnIcon.classList.remove('animate')
-    }, 500)
-}
-
-/* add shake animation to the inputs container */
-function shakeAnimation() {
-    inputsContainer.classList.add('shake');
-
-    // Remove the class after animation ends
-    setTimeout(() => {
-        inputsContainer.classList.remove('shake');
-    }, 500); // matches animation duration
+    }, 1500);
 }
